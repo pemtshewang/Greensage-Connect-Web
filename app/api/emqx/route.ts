ea7600d5b144757a99f54522f2952f939f262df3
pemtshewang <pemtshewang500@gmail.com>
2024-05-07 02:53:42 +0600

    added

--- app/api/emqx/route.ts
+++ app/api/emqx/route.ts
@@ -3,9 +3,10 @@ import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   const mqttService = new MQTTServiceHandler();
-  const { topic, payload } = await request.json();
+  const text = await request.text();
+  const parseJson = JSON.parse(text);
   try {
-    await mqttService.handleMQTTMessage(topic, payload);
+    await mqttService.handleMQTTMessage(parseJson.topic, parseJson.payload);
     return NextResponse.json({ message: "ok" }, { status: 200 });
   } catch (err) {
     return NextResponse.json({ message: "ok" }, { status: 400 });
