2efc065377935ab0d2b6d59ceb463ef15044bcb8
pemtshewang <pemtshewang500@gmail.com>
2024-05-07 02:37:01 +0600

    added data ingestion

---
+++ app/api/emqx/mqtt.service.ts
@@ -0,0 +1,86 @@
+import { db } from "@/lib/db";
+
+export class MQTTServiceHandler {
+  async handleMQTTMessage(topic: string, payload: string) {
+    this.identifyTopics(topic, payload);
+  }
+
+  identifyTopics(topic: string, payload: string) {
+    const [identifier, ...topicFormatted] = topic.split("/");
+
+    switch (identifier) {
+      case "user":
+        this.handleUserMessage(topicFormatted, payload);
+        break;
+      default:
+        break;
+    }
+  }
+
+  private async handleUserMessage(topic: string[], payload: string) {
+    const [, controllerId, actionIdentifier, ...action] = topic;
+
+    switch (actionIdentifier) {
+      case "readings":
+        const readings = payload.split("|");
+        const readingData = readings.reduce((acc, item) => {
+          const [type, value] = item.split(":");
+          acc[type] = parseFloat(value);
+          return acc;
+        }, {});
+
+        await db.reading.create({
+          data: {
+            controllerId,
+            ...readingData,
+          },
+        });
+        console.log("updated");
+        break;
+      case "threshold":
+        const type = action[0];
+        const value = parseFloat(payload);
+
+        switch (type) {
+          case "temperature":
+            await db.temperatureThresholdRecord.create({
+              data: {
+                controllerId,
+                value,
+              },
+            });
+            break;
+          case "soilMoisture":
+            await db.soilMoistureThresholdRecord.create({
+              data: {
+                controllerId,
+                value,
+              },
+            });
+            break;
+          case "humidity":
+            await db.humidityThresholdRecord.create({
+              data: {
+                controllerId,
+                value,
+              },
+            });
+            break;
+        }
+        break;
+      case "wschedule":
+        const [startTime, endTime, repetitionDays] = payload.split("|");
+        await db.waterScheduleRecord.create({
+          data: {
+            controllerId,
+            startTime,
+            endTime,
+            repetitionDays: Number(repetitionDays),
+          },
+        });
+        break;
+      default:
+        break;
+    }
+  }
+}
---
+++ app/api/emqx/route.ts
@@ -0,0 +1,13 @@
+import { MQTTServiceHandler } from "./mqtt.service";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const mqttService = new MQTTServiceHandler();
+  const { topic, payload } = await request.json();
+  try {
+    await mqttService.handleMQTTMessage(topic, payload);
+    return NextResponse.json({ message: "ok" }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ message: "ok" }, { status: 400 });
+  }
+}
