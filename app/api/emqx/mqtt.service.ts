1a50baf6f1410200fcfaa85b2d20e19b60713915
pemtshewang <pemtshewang500@gmail.com>
2024-05-07 09:45:10 +0600

    debgu

--- app/api/emqx/mqtt.service.ts
+++ app/api/emqx/mqtt.service.ts
@@ -19,7 +19,7 @@ export class MQTTServiceHandler {
 
   private async handleUserMessage(topic: string[], payload: string) {
     const [userBrokerId, controllerId, actionIdentifier, ...action] = topic;
-    console.log(controllerId);
+    console.log(userBrokerId, controllerId, actionIdentifier, action);
 
     switch (actionIdentifier) {
       case "readings":
