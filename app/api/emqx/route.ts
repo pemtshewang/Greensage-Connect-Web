3a55ffa32fbe4d08e42fe16319f8e99b83288aa2
pemtshewang <pemtshewang500@gmail.com>
2024-05-07 10:24:48 +0600

    added files

--- app/api/emqx/route.ts
+++ app/api/emqx/route.ts
@@ -4,6 +4,7 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
   const mqttService = new MQTTServiceHandler();
   const { topic, payload } = await request.json();
+  console.log(topic, payload);
   try {
     await mqttService.handleMQTTMessage(topic, payload);
     return NextResponse.json({ message: "ok" }, { status: 200 });
